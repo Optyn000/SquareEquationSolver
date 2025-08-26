ab0a43155705bac1e916b3ce47895a9021130292
Optyn000 <glebuntilov@gmail.com>
2025-08-26 20:49:44 +0300

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <TXLib.h>
+#include <math.h>
+#include <assert.h>
+
+double DiscriminantSolver(double a, double b, double c, double* discriminant);
+int EquationSolver(double a, double b, double c, double* x1, double* x2, int* nRoots);
+int if_equal(double num1, double num2);
+void Input(double* a, double* b, double* c);
+
+const double EPSILON = 0.000001;
+
+
+int main()
+{
+    double a, b, c = NAN;
+    double x1, x2 = NAN;
+    int nRoots = NAN;
+
+    Input(&a, &b, &c);
+    EquationSolver(a, b, c, &x1, &x2, &nRoots);
+
+    if (if_equal(a, 0) && if_equal(b, 0) && !if_equal(c, 0))
+    {
+        printf("Введено неверное тождество\n");
+        return 0;
+    }
+
+    if (if_equal(a, 0) && if_equal(b, 0) && if_equal(c, 0))
+    {
+        printf("Корнями уравнения являются любые действительные числа\n");
+        return 0;
+    }
+
+    if (!if_equal(a, 0) && if_equal(b, 0) && if_equal(c, 0))
+    {
+        printf("Уравнение имеет единственный корень. x = 0\n");
+        return 0;
+    }
+
+    if (if_equal(nRoots, 1) && isnan(x2))
+    {
+        printf("Уравнение является линейным. x = %lf\n", x1);
+        return 0;
+    }
+
+    if isnan(x1)
+    {
+        printf("Уравнение не имеет действительных корней\n");
+        return 0;
+    }
+
+    if (if_equal(x1, x2))
+    {
+        printf("Уравнение имеет один корень двойной кратности. x = %lf\n", x1);
+        return 0;
+    }
+
+    if (if_equal(nRoots, 2))
+    {
+        printf("Уравнение имеет два различных действительных корня. x1 = %lf, x2 = %lf\n", x1, x2);
+        return 0;
+    }
+}
+
+
+void Input(double* a, double* b, double* c)
+{
+    int CoeffCount = 0;
+
+    printf("Решение квадратного уравнения\nПоследовательно введите коэффициенты при x через пробел\n");
+    CoeffCount = scanf("%lf %lf %lf", a, b, c);
+
+    if (!(CoeffCount == 3))
+    {
+        printf("Видимо, вы неправильно ввели коэффициенты уравнения. Извините, программа не работает");
+    }
+}
+
+
+enum results
+{
+    LinearEquation,
+    NoRoots,
+    OneRootOfDoubleMultiplicity,
+    TwoRoots,
+    AnotherCase
+};
+
+
+int EquationSolver(double a, double b, double c, double* x1, double* x2, int* nRoots)
+{
+
+    assert(x1);
+    assert(x2);
+
+    double discriminant = NAN;
+    enum results res;
+
+
+    if (if_equal(a, 0) && !if_equal(b, 0))
+    {
+        *x1 = -c / b;
+        *x2 = NAN;
+        *nRoots = 1;
+        res = LinearEquation;
+        return 0;
+    }
+
+    DiscriminantSolver(a, b, c, &discriminant);
+
+    if (discriminant < 0)
+    {
+        *x1 = NAN;
+        *x2 = NAN;
+        *nRoots = 0;
+        res = NoRoots;
+        return 0;
+    }
+
+    if (if_equal(discriminant, 0) && !if_equal(a, 0))
+    {
+        *x1 = *x2 = -b / (2*a);
+        *nRoots = 1;
+        res = OneRootOfDoubleMultiplicity;
+        return 0;
+    }
+
+    if (discriminant > 0 && !if_equal(a, 0))
+    {
+        double sqrt_discriminant = sqrt(discriminant);
+        *x1 = (-b + sqrt_discriminant) / (2*a);
+        *x2 = (-b - sqrt_discriminant) / (2*a);
+        *nRoots = 2;
+        res = TwoRoots;
+        return 0;
+    }
+
+    res = AnotherCase;
+    return 0;
+}
+
+
+double DiscriminantSolver(double a, double b, double c, double* discriminant)
+{
+    *discriminant = ((b * b) - 4 * a * c);
+    return 0;
+}
+
+
+int if_equal(double num1, double num2)
+{
+    return fabs(num1 - num2) < EPSILON;
+}
---
+++ square_test.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <TXLib.h>
+#include <math.h>
+
+struct TestData
+{
+double a, b, c;
+double x1, x2;
+int nRoots;
+};
+
+
+int EquationSolver(double a, double b, double c, double* x1, double* x2, int* nRoots);
+void PassedCount();
+int OneTest(TestData test);
+void AllTests();
+
+
+void PassedCount()
+{
+    int passed = 0;
+    AllTests();
+    printf("Верно решённых тестов: %d\n", passed);
+}
+
+
+int OneTest(TestData test)
+{
+    double x1, x2 = 0;
+
+    if (EquationSolver(test.a, test.b, test.c, &x1, &x2, &nRoots) == TwoRoots ||
+    EquationSolver(test.a, test.b, test.c, &x1, &x2, &nRoots) == OneRootOfDoubleMultiplicity)
+    {
+        if (test.x1 == x1 && test.x2 == x2)    //  (if_equal(tests[i].x1, x1) && if_equal(tests[i].x2, x2));
+        {
+             return 1;
+        }
+        return 0;
+    }
+
+
+    if (EquationSolver(test.a, test.b, test.c, &x1, &x2, &nRoots) == NoRoots)
+    {
+        if (isnan(x1) && isnan(x2))
+        {
+             return 1;
+        }
+        return 0;
+    }
+
+
+    if (EquationSolver(test.a, test.b, test.c, &x1, &x2, &nRoots) == LinearEquation)
+    {
+        if (test.x1 == x1 && isnan(x2))   //  if_equal(tests[i].x1, x1);
+        {
+             return 1;
+        }
+        return 0;
+    }
+
+    return 0;
+}
+
+
+void AllTests()
+{
+    int passed = 0;
+
+    TestData tests[] = {
+    {.a = 1, .b = -5, .c = 6, .x1 = 2, .x2 = 3},
+    {.a = 1, .b = 2, .c = 1, .x1 = -1, .x2 = -1},
+    {.a = 1, .b = -2, .c = 1, .x1 = 1, .x2 = 1},
+    {.a = 1, .b = -11, .c = 30, .x1 = 5, .x2 = 6},
+    };
+
+
+    int size = sizeof(tests) / sizeof(tests[0]);
+
+    for (int i = 0; i < size; i++)
+    {
+        passed += OneTest(tests[i]);
+        if (!OneTest(tests[i]))
+        {
+            printf("FAILED TEST №%d: a: %lf, b: %lf, c: %lf.\n", i, tests[i].a, tests[i].b, tests[i].c);
+        }
+    }
+}
+
